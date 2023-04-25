fb700d72b3f2e219108b1dd6411ed85391b00752
shikha1970 <SHAGRAWAL@MICROSOFT.COM>
2023-04-25 09:29:27 +0530

    Update train.py

--- code/train/train.py
+++ code/train/train.py
@@ -15,7 +15,7 @@ from sklearn.model_selection import train_test_split
 from azureml.core import Dataset, Run
 run = Run.get_context() 
 
-
+# Adding dummy comment
 def log_confusion_matrix_image(cm, labels, normalize=False, log_name='confusion_matrix', title='Confusion matrix', cmap=plt.cm.Blues):
     '''
     This function prints and plots the confusion matrix.
